8f74c0b5ed6ba347273530d27520a53b2bd68219
Carlos Daniel <carlosporrasayala@gmail.com>
2024-02-28 17:03:40 -0500

    Add functionality to the By Country and By Region pages

--- src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -1,8 +1,20 @@
 import { Component } from '@angular/core';
+import { Country } from '../../interfaces/country';
+import { CountriesService } from '../../services/countries.service';
 
 @Component({
   selector: 'countries-by-country-page',
   templateUrl: './by-country-page.component.html',
   styles: ``,
 })
-export class ByCountryPageComponent {}
+export class ByCountryPageComponent {
+  countries: Country[] = [];
+
+  constructor(private countriesService: CountriesService) {}
+
+  searchCountry(term: string): void {
+    this.countriesService
+      .searchCountry(term)
+      .subscribe((countries) => (this.countries = countries));
+  }
+}
