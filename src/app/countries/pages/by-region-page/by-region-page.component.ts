1a57f1e17153efd47cd0caf66026328afa80dbbb
Carlos Daniel <carlosporrasayala@gmail.com>
2024-03-04 18:06:12 -0500

    Optimize ByRegionPage

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/country';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'africa' | 'americas' | 'asia' | 'europe' | 'oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -9,12 +11,17 @@ import { CountriesService } from '../../services/countries.service';
 })
 export class ByRegionPageComponent {
   countries: Country[] = [];
+  regions: Region[] = ['africa', 'americas', 'asia', 'europe', 'oceania'];
+  selectedRegion?: Region;
 
   constructor(private countriesService: CountriesService) {}
 
-  searchregion(term: string): void {
+  searchregion(region: Region): void {
+    if (this.selectedRegion === region) return;
+
+    this.selectedRegion = region;
     this.countriesService
-      .searchRegion(term)
+      .searchRegion(region)
       .subscribe((countries) => (this.countries = countries));
   }
 }
